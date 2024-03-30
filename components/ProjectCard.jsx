5eef04e7b1dc22fe19d6e28e21f15ed5ab654203
AzRom <azromjr@gmail.com>
2024-03-30 22:46:38 +0300

    another commit

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -23,7 +23,7 @@ const ProjectCard = ({
       viewport={{ once: true }}
       className="group border-2 border-[#33415579] hover:rounded-none text-white transform translate duration-300 hover:scale-105 hover:shadow-xl"
     >
-      <div className="h-72 relative group">
+      <div className="h-52 md:h-72 relative group">
         <div
           className="absolute top-0 left-0 w-full h-full group-hover:opacity-0 transform transition-all duration-[1s] z-40"
           style={{ background: `url(${imgOne})`, backgroundSize: "cover" }}
@@ -44,10 +44,10 @@ const ProjectCard = ({
           className="absolute top-0 left-0 w-full h-full z-0"
           style={{ background: `url(${imgFive})`, backgroundSize: "cover" }}
         ></div>
-        <div className="z-50 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] rounded-full w-[180px] h-[75px] bg-[#212529] group-hover:shadow-xl opacity-0 group-hover:opacity-70 transition-opacity duration-500 flex justify-center items-center">
+        <div className="z-50 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] rounded-full w-[120px] md:w-[180px] h-[60px] md:h-[75px] bg-[#212529] group-hover:shadow-xl opacity-0 group-hover:opacity-70 transition-opacity duration-500 flex justify-center items-center">
           <Link
             href={gitUrl}
-            className="w-14 h-14 mr-12 rounded-full border-2 relative border-slate-400 hover:border-white hover:text-white flex justify-center items-center group/link"
+            className="w-10 md:w-14 h-10 md:h-14 mr-6 lg:mr-12 rounded-full border-2 relative border-slate-400 hover:border-white hover:text-white flex justify-center items-center group/link"
           >
             <CodeBracketIcon className="w-8 h-8 text-slate-400 group-hover/link:text-white cursor-pointer" />
             <span className="hidden group-hover/link:flex text-gray-400 bg-black py-2 px-4 absolute -top-12 rounded-xl">
@@ -57,7 +57,7 @@ const ProjectCard = ({
 
           <Link
             href={previewUrl}
-            className="w-14 h-14 rounded-full border-2 relative border-slate-400 hover:border-white hover:text-white flex justify-center items-center group/link"
+            className="w-10 md:w-14 h-10 md:h-14 rounded-full border-2 relative border-slate-400 hover:border-white hover:text-white flex justify-center items-center group/link"
           >
             <EyeIcon className="w-8 h-8 text-slate-400 group-hover/link:text-white cursor-pointer" />
             <span className="hidden group-hover/link:flex text-gray-400 bg-black py-2 px-4 absolute -top-12 rounded-xl transform transition-all duration-[1s]">
